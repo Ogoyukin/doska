7d88c41f9ea18b08562db0ddca69eeb024b3d0c8
Ogoyukin <Ogoyukin93@gmail.com>
2022-02-19 15:45:49 +0900

    Добавлены комментарии для наглядноости

--- main.go
+++ main.go
@@ -13,14 +13,30 @@ import (
 var db *sql.DB
 
 func main() {
-	dataSource, err := sql.Open("mysql", "root:root@/doska")
 
-	db = dataSource
+	// Устанавливаю соединениес Базой данных MySQL
+	db, err := sql.Open("mysql", "root:root@/doska")
+
+	// Проверяю есть ли ошибки
 	if err != nil {
-		log.Fatal(err)
+
+		// Если есть ошибка, останавливаю приложение с ошибкой
+		log.Fatalf("Не могу подключиться к БД: %v", err)
 	}
 
+	// Проверяю соединение
+	err = db.Ping()
+
+	// Проверяю есть ли ошибки
+	if err != nil {
+		// Если есть ошибка, останавливаю приложение с ошибкой
+		log.Fatalf("Не могу дойти до БД: %v", err)
+	}
+
+	// Создаю обработчик запросов
 	http.HandleFunc("/hello", Handler)
+
+	// Запускаю http server
 	http.ListenAndServe(":8090", nil)
 }
 
@@ -31,25 +47,41 @@ type Params struct {
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 
+	// Подключаю корневой шаблон страницы
 	tmp, _ := template.ParseFiles("templates/index.html")
-	tmp.ParseGlob("templates/*.html")
 
-	products := []pkg.Item{}
+	// Сканирую другие шаблоны
+	tmp, err := tmp.ParseGlob("templates/*.html")
 
+	items := []pkg.Item{}
+
+	// Отправляю запрос в БД, достать данные из таблицы items в базе doska
 	rows, err := db.Query("select * from doska.items")
+
+	// Проверяю результат запроса на предмет ошибкок
 	if err != nil {
-		panic(err)
+		// Если есть ошибка, останавливаю приложение с ошибкой
+		log.Fatalf("Не могу выполнить запрос: %v", err)
 	}
+
+	// Закрываю запрос, как только функция выполнится, ключевое слово "defer"
 	defer rows.Close()
+
+	// Скаинрую ответ от БД
 	for rows.Next() {
 		p := pkg.Item{}
+
+		// Присваию ответы в модели item
 		err := rows.Scan(&p.ID, &p.Price, &p.Title, &p.Description, &p.Img)
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-		products = append(products, p)
+
+		// Добавляю элемент в массив
+		items = append(items, p)
 	}
 
-	tmp.Execute(w, Params{Items: products})
+	// Передаю шаблону необходимые параметры
+	tmp.Execute(w, Params{Items: items})
 }
