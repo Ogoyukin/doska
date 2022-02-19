bfec95af44d9a019a5985b892585ede256b2749f
Ogoyukin <Ogoyukin93@gmail.com>
2022-02-19 14:50:28 +0900

    Создал таблицу items

--- migration/1.sql
+++ migration/1.sql
@@ -1,1 +1,11 @@
 CREATE DATABASE doska CHARACTER SET UTF8mb4 COLLATE utf8mb4_unicode_ci;
+
+create table items
+(
+    id int auto_increment primary key,
+    price int,
+    title varchar(255),
+    description varchar(255),
+    img varchar(255)
+);
+
