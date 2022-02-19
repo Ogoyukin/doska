c4d0d5ebbe345ece607555bd8ab2995da9d34de9
Ogoyukin <Ogoyukin93@gmail.com>
2022-02-19 14:38:17 +0900

    Передача массива объектов

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/Ogoyukin/doska/pkg"
 	"html/template"
 	"net/http"
 )
@@ -14,7 +15,7 @@ var counter = 0
 
 type Params struct {
 	Counter int
-	Name    string
+	Items   []pkg.Item
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -23,10 +24,28 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	tmp, _ := template.ParseFiles("templates/index.html")
 	tmp.ParseGlob("templates/*.html")
 
-	name := r.URL.Query().Get("name")
+	items := []pkg.Item{{
+		ID:          1,
+		Price:       50000,
+		Title:       " Продаётся iPhone 13",
+		Description: "Продаётся iPhone 13 Pro пользовался только неделю, в отл состояниее акум 99%. Только сегодня...",
+		Img:         "https://img.mvideo.ru/Pdb/30059037b.jpg",
+	}}
 
-	tmp.Execute(w, Params{
-		Counter: counter,
-		Name:    name,
-	})
+	for i := 0; i < 20; i++ {
+		img := "https://telefonplus.ru/wp-content/uploads/2020/10/iphone-12-blue-select-2020.png"
+
+		if i%2 == 0 {
+			img = "https://img.mvideo.ru/Pdb/30059037b.jpg"
+		}
+		items = append(items, pkg.Item{
+			ID:          1,
+			Price:       50000,
+			Title:       " Продаётся iPhone 12",
+			Description: "Продаётся iPhone 12  пользовался только неделю, в отл состояниее акум 99%. Только сегодня...",
+			Img:         img,
+		})
+	}
+
+	tmp.Execute(w, Params{Items: items})
 }
